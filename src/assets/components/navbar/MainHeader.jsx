56d558af94f42c70e6c7151db7d849e22d54075d
[Agustin-Corvalan] <[agustin.corvalan@hotmail.com]>
2023-04-20 20:40:47 -0300

    pequeñas modificaciones

--- src/assets/components/navbar/MainHeader.jsx
+++ src/assets/components/navbar/MainHeader.jsx
@@ -1,5 +1,5 @@
 import LogoTribu from "@/assets/images/marca-tribu.svg";
-import Avatar from "@/assets/images/image-avatar.png";
+import Avatar from "@/assets/images/avatar-sustentable.png";
 import MenuIcon from "@/assets/components/icons/MenuIcon";
 import CartIcon from "@/assets/components/icons/CartIcon";
 import CloseIcon from "@/assets/components/icons/CloseIcon";
@@ -12,20 +12,22 @@ const MainHeader = () => {
     );
 
     const handleOpenMenu = () => {
-        setNavClass("absolute top-0 left-0 flex h-full w-4/5 flex-col  gap-y-[21px] bg-white p-8 font-bold  md:static md:mr-auto md:flex md:flex-row md:gap-4 md:p-0 md:h-auto"
+        setNavClass("absolute top-0 left-0 flex h-full w-3/5 flex-col  gap-y-[21px] bg-gray-400 p-2 font-bold  md:static md:mr-auto md:flex md:flex-row md:gap-4 md:p-0 md:h-auto"
         );
     };
     const handleCloseMenu = () => {
-        setNavClass("hidden font-bold  md:static md:mr-auto md:flex md:h-auto md:flex-row md:gap-4 md:p-0");
+        setNavClass("hidden font-bold md:static md:mr-auto md:flex md:h-auto md:flex-row md:gap-4 md:p-0");
     };
     return (
 
-        <header className=" container mx-auto flex items-center gap-8 bg-gray-200 px-4 py-8">
-            <button className="md:hidden" onClick={handleOpenMenu}>
+        <header className=" container mx-auto flex items-center gap-8 bg-white px-2 py-8">
+            
+            <button className="md:hidden"  onClick={handleOpenMenu} >
                 <MenuIcon />
+                
             </button>
 
-            <img src={LogoTribu} alt="Logo Tribu" className="mr-auto mb-1 h-5 md:mr-0 " />
+            <img src={LogoTribu} alt="Logo Tribu" className="mr-auto mb-1 h-8 md:mr-0 " />
 
             <nav className={navClass}>
                 <button className="mb-12 md:hidden" onClick={handleCloseMenu} >
@@ -36,14 +38,17 @@ const MainHeader = () => {
                 <a href="#">Mi tienda</a>
                 <a href="#">Vender</a>
                 <a href="#">Blog</a>
-                <a href="#">Carrito</a>
+                
             </nav>
 
             <div className="flex gap-4">
                 <button className="">
                     <CartIcon />
                 </button>
-                <img src={Avatar} alt="" className="w-9" />
+                <p id="Sign up">Sign up</p>
+                <a href="#"><img src={Avatar} alt="sign up" className="static w-9 "/></a>
+                
+
             </div>
 
         </header>
